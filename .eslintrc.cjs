456392e08ef2cc9b8cea055254c9cb7edfe66b95
xpadev <xpadev@gmail.com>
2024-02-07 20:45:25 +0900

    chore: add husky and lint-staged

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -8,11 +8,19 @@ module.exports = {
   ],
   ignorePatterns: ['dist', '.eslintrc.cjs'],
   parser: '@typescript-eslint/parser',
-  plugins: ['react-refresh'],
+  plugins: ['react-refresh',"simple-import-sort"],
   rules: {
     'react-refresh/only-export-components': [
       'warn',
       { allowConstantExport: true },
     ],
+    "@typescript-eslint/restrict-template-expressions": "off",
+    "@typescript-eslint/unbound-method": "off",
+    "@typescript-eslint/no-unused-vars": "error",
+    "@next/next/no-img-element": "off",
+    "no-unused-vars": "off",
+    "no-control-regex": "off",
+    "simple-import-sort/imports": "error",
+    "simple-import-sort/exports": "error"
   },
 }

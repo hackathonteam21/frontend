a0199f0d113dabf184f44c519c2497d48320c830
XPA <xpadev@gmail.com>
2024-02-09 20:34:13 +0900

    [wip] feat: contexts

--- src/App.tsx
+++ src/App.tsx
@@ -1,37 +1,20 @@
-import "./App.css";
+import styles from "./App.module.css";
 
-import { useState } from "react";
-
-import viteLogo from "/vite.svg";
-
-import reactLogo from "./assets/react.svg";
+import { useContext } from "react";
+import { RouteContext } from "./context.tsx";
 
 function App() {
-  const [count, setCount] = useState(0);
+  const context = useContext(RouteContext);
+  if (!context) return;
+  const [route, setRoute] = context;
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <div
+      className={styles.wrapper}
+      onClick={() => setRoute([{ text: "Home" }])}
+    >
+      {JSON.stringify(route)}
+    </div>
   );
 }
 

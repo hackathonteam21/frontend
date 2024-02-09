d201458e50808353aea27ed80cbb292cdb2690ce
wsdnakamoto <wsdnakamoto@gmail.com>
2024-02-09 20:20:25 +0900

    initial commit

--- src/main.tsx
+++ src/main.tsx
@@ -4,9 +4,11 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 
 import App from "./App.tsx";
+import AddressManager from "./AddressManager.tsx";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <App />
+    <AddressManager />
   </React.StrictMode>,
 );

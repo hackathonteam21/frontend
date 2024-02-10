3a7e8dff6f55cec2fec1bdc92c954f86ac1a232f
xpadev <xpadev@gmail.com>
2024-02-10 14:51:12 +0900

    feat: route service

--- src/map/map.tsx
+++ src/map/map.tsx
@@ -3,34 +3,35 @@ import { GoogleMap, useJsApiLoader } from "@react-google-maps/api";
 import { Direction } from "./direction";
 import styles from "./map.module.scss";
 import { useContext } from "react";
-import { RouteContext } from "../context.tsx";
+import { RouteContext, SettingsContext } from "../context.tsx";
 
 const center = {
   lat: -3.745,
   lng: -38.523,
 };
 
-const destination = {
-  lat: 35.5020314,
-  lng: 138.448022,
-};
-
 const MainMap = () => {
-  const context = useContext(RouteContext);
+  const routeContext = useContext(RouteContext);
+  const settingContext = useContext(SettingsContext);
   const { isLoaded } = useJsApiLoader({
     id: "google-map-script",
     googleMapsApiKey: import.meta.env.VITE_GOOGLE_MAPS_API_KEY as string,
   });
-  if (!isLoaded || !context) return <div>loading...</div>;
-  const [route] = context;
+  if (!isLoaded || !routeContext || !settingContext)
+    return <div>loading...</div>;
+  const [route] = routeContext;
+  const [settings] = settingContext;
+
   return (
     <div className={styles.wrapper}>
       <GoogleMap mapContainerClassName={styles.map} center={center} zoom={10}>
-        <Direction
-          origin={destination}
-          transitPoints={route.map((pos) => pos.location)}
-          destination={destination}
-        />
+        {settings.origin && settings.destination && (
+          <Direction
+            origin={settings.origin.location}
+            transitPoints={route.map((pos) => pos.location)}
+            destination={settings.destination.location}
+          />
+        )}
       </GoogleMap>
     </div>
   );
